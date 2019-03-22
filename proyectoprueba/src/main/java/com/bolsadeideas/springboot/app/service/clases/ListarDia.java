362420ac99e8be3d89e865c5bbe71e49613269f9
usuario <usuario@DESKTOP-7813UQ9>
2019-03-22 10:15:19 -0500

    PROBANDO RAMAS DE GITHUB

--- proyectoprueba/src/main/java/com/bolsadeideas/springboot/app/service/clases/ListarDia.java
+++ proyectoprueba/src/main/java/com/bolsadeideas/springboot/app/service/clases/ListarDia.java
@@ -1,8 +1,11 @@
 package com.bolsadeideas.springboot.app.service.clases;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bolsadeideas.springboot.app.controller.listardiacontroller;
 import com.bolsadeideas.springboot.app.entity.Dia;
 import com.bolsadeideas.springboot.app.repository.diarepository;
 import com.bolsadeideas.springboot.app.service.interfaces.IlistarDia;
@@ -12,10 +15,12 @@ public class ListarDia implements IlistarDia {
 
 	@Autowired
 	private diarepository diarepository;
-	
+	private static final Log Logger = LogFactory.getLog(ListarDia.class);
+
 	
 	@Override
 	public Iterable<Dia> listardia() {
+		Logger.info("NOS ENCONTRAMOS EN EL SERVICE LISTADIA");		
 		return   diarepository.findAll();
 	}
 
